626c37c8ec04fd506b90d55001e818af93b83b29
Victor Laio <contatodev.laio@gmail.com>
2025-09-17 21:24:03 -0300

    primeiro commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+type Todo = {
+  id: number;
+  text: string;
+};
+
+export const TodoList = () => {
+  const todos: Todo[] = [
+    { id: 1, text: "Estudar React" },
+    { id: 2, text: "Estudar JS" },
+    { id: 3, text: "Estudar TypeScript" },
+  ];
+
+  return (
+    <>
+      <h1>Minhas Tarefas</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
